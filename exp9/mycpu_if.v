3754751b9131ab3d670aa06c49afd55572f911ce
Horan-Z <i17611745217@gmail.com>
2026-05-01 18:13:49 +0800

    添加exp9代码

---
+++ exp9/mycpu_if.v
@@ -0,0 +1,40 @@
+module mycpu_if(
+    input  wire        clk_i,
+    input  wire        reset_i,
+    input  wire        id_allowin_i,
+    input  wire        valid_i,
+    input  wire [31:0] pc_i,
+    input  wire        br_taken_cancel_i,
+    output wire        if_to_id_valid_o,
+    output wire        if_allowin_o,
+
+    input  wire [31:0] inst_sram_rdata_i,
+
+    output wire [31:0] inst_o,
+    output wire [31:0] pc_o
+);
+
+wire        if_ready_go = 1'b1;
+reg         reg_valid;
+reg  [31:0] reg_pc; 
+reg  [31:0] reg_inst;
+
+always @(posedge clk_i) begin
+    if (reset_i) begin
+        reg_valid <= 1'b0;
+    end else if(if_allowin_o) begin
+        reg_valid <= valid_i;
+        reg_pc    <= pc_i;
+        reg_inst  <= inst_sram_rdata_i;
+    end else if(br_taken_cancel_i) begin
+        reg_valid <= 1'b0;
+    end
+end
+
+assign inst_o           = reg_inst;
+assign pc_o             = reg_pc;
+
+assign if_allowin_o = !reg_valid || (if_ready_go && id_allowin_i);
+assign if_to_id_valid_o = reg_valid & if_ready_go;
+
+endmodule
---
+++ exp9/mycpu_mem.v
@@ -0,0 +1,69 @@
+module mycpu_mem(
+    input  wire        clk_i,
+    input  wire        reset_i,
+
+    input  wire [31:0] pc_i,
+    input  wire        gr_we_i,
+    input  wire        res_from_mem_i,
+    input  wire [31:0] alu_result_i,
+    input  wire [ 4:0] dest_i,
+    input  wire [31:0] data_sram_rdata_i,
+
+    output wire [31:0] pc_o,
+    output wire        gr_we_o,
+    output wire        res_from_mem_o,
+    output wire [31:0] alu_result_o,
+    output wire [ 4:0] dest_o,
+    output wire [31:0] mem_result_o,
+
+    input  wire        valid_i,
+    input  wire        wb_allowin_i,
+    output wire        mem_allowin_o,
+    output wire        mem_to_wb_valid_o
+);
+
+wire       mem_ready_go = 1'b1;
+
+reg [31:0] reg_pc;
+reg        reg_valid;
+reg        reg_gr_we;
+reg        reg_res_from_mem;
+reg [31:0] reg_alu_result;
+reg [ 4:0] reg_dest;
+
+always @(posedge clk_i) begin
+    if (reset_i) begin
+        reg_valid <= 1'b0;
+    end else if(mem_allowin_o) begin
+        reg_valid <= valid_i;
+    end
+end
+
+always @(posedge clk_i) begin
+    if(mem_allowin_o && valid_i) begin
+        reg_pc           <= pc_i;
+        reg_gr_we        <= gr_we_i;
+        reg_res_from_mem <= res_from_mem_i;
+        reg_alu_result   <= alu_result_i;
+        reg_dest         <= dest_i;
+    end
+end
+
+// mem级流水只负责把读取的数据转发给下一级（逻辑上），对sram的操作由上一级ex进行
+// 实际上，还需要通过mem_allowin对上一级ex流水线进行控制
+// 具体表现为如果后面流水阻塞，需要通过控制信号保证对sram的操作仅执行一次
+// 对sram的操作应发生在ex_to_mem_valid拉高的那一个时刻，即ex流水转向mem流水的时刻
+assign mem_result_o   = data_sram_rdata_i;
+
+// 中转信号
+assign pc_o           = reg_pc;
+assign gr_we_o        = reg_gr_we;
+assign res_from_mem_o = reg_res_from_mem;
+assign alu_result_o   = reg_alu_result;
+// 如果不是valid，就释放dest，防止死锁
+assign dest_o         = reg_valid ? reg_dest : 5'd0;
+
+assign mem_allowin_o = !reg_valid || (mem_ready_go && wb_allowin_i);
+assign mem_to_wb_valid_o = reg_valid && mem_ready_go;
+
+endmodule
