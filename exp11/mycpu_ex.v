63746ad40a1f20e42e8da72ce5281ad31f70f74a
Horan-Z <i17611745217@gmail.com>
2026-05-02 15:20:20 +0800

    添加exp11代码

---
+++ exp11/mycpu_ex.v
@@ -0,0 +1,157 @@
+module mycpu_ex(
+    input  wire        clk_i,
+    input  wire        reset_i,
+
+    // 输入
+    input  wire [31:0] pc_i,
+    input  wire        gr_we_i,
+    input  wire        res_from_mem_i,
+    input  wire [ 4:0] dest_i,
+    input  wire [18:0] ex_op_i,
+    input  wire [31:0] ex_src1_i,
+    input  wire [31:0] ex_src2_i,
+    input  wire        mem_en_i,
+    input  wire [ 3:0] mem_we_i,
+    input  wire [31:0] rkd_value_i,
+
+    // 给下一级的数据
+    output wire [31:0] pc_o,
+    output wire        gr_we_o,
+    output wire        res_from_mem_o,
+    output wire [ 4:0] dest_o,
+    output wire [31:0] ex_result_o,
+
+    // 流水线前递用
+    // 标记ex阶段无法获得待写入的寄存器值
+    input  wire        ex_not_ready_i,
+    output wire        ex_not_ready_o,
+
+    // 控制信号
+    input  wire        valid_i,
+    input  wire        mem_allowin_i,
+    output wire        ex_allowin_o,
+    output wire        ex_to_mem_valid_o,
+
+    // data_ram读写信号
+    output wire        data_sram_en_o,
+    output wire [ 3:0] data_sram_we_o,
+    output wire [31:0] data_sram_addr_o,
+    output wire [31:0] data_sram_wdata_o
+);
+
+wire ex_ready_go = (reg_div_op != 4'd0) ? (div_done | div_complete) : 1'b1;
+
+wire [31:0] alu_result;
+wire [31:0] mul_result;
+wire [31:0] div_result;
+wire        div_start;
+wire        div_done;
+
+reg [31:0] reg_pc;
+reg        reg_valid;
+reg        reg_gr_we;
+reg        reg_res_from_mem;
+reg [ 4:0] reg_dest;
+reg [11:0] reg_alu_op;
+reg [ 2:0] reg_mul_op;
+reg [ 3:0] reg_div_op;
+reg [31:0] reg_cal_src1;
+reg [31:0] reg_cal_src2;
+reg [31:0] reg_mul_src1;
+reg [31:0] reg_mul_src2;
+reg        reg_mem_en;
+reg [ 3:0] reg_mem_we;
+reg [31:0] reg_rkd_value;
+reg        reg_ex_not_ready;
+
+always @(posedge clk_i) begin
+    if (reset_i) begin
+        reg_valid <= 1'b0;
+    end else if(ex_allowin_o) begin
+        reg_valid <= valid_i;
+    end
+end
+
+always @(posedge clk_i) begin
+    if(ex_allowin_o && valid_i) begin
+        reg_pc           <= pc_i;
+        reg_gr_we        <= gr_we_i;
+        reg_res_from_mem <= res_from_mem_i;
+        reg_dest         <= dest_i;
+        reg_alu_op       <= ex_op_i[11:0];
+        reg_mul_op       <= ex_op_i[14:12];
+        reg_div_op       <= ex_op_i[18:15];
+        reg_cal_src1     <= ex_src1_i;
+        reg_cal_src2     <= ex_src2_i;
+        reg_mul_src1     <= ex_src1_i;
+        reg_mul_src2     <= ex_src2_i;
+        reg_mem_en       <= mem_en_i;
+        reg_mem_we       <= mem_we_i;
+        reg_rkd_value    <= rkd_value_i;
+        reg_ex_not_ready <= ex_not_ready_i;
+    end
+end
+
+reg div_complete;
+always @(posedge clk_i) begin
+    if (reset_i) begin              // 高电平同步复位
+        div_complete <= 1'b0;
+    end else if (reg_valid && ex_ready_go && mem_allowin_i) begin
+        div_complete <= 1'b0;       // 握手成功，指令离开 EX 级，清零记忆
+    end else if (div_done) begin
+        div_complete <= 1'b1;       // 除法算完但走不掉，记忆完成状态
+    end
+end
+
+assign div_start = reg_valid && (reg_div_op != 4'd0) && !div_complete;
+
+// 中转信号
+assign pc_o           = reg_pc;
+assign gr_we_o        = reg_gr_we;
+assign res_from_mem_o = reg_res_from_mem;
+
+// 如果不是valid，就释放dest，防止死锁
+assign dest_o         = reg_valid ? reg_dest : 5'd0;
+// 不需要判断reg_gr_we，已在ID阶段处理，如果不需要写入此处dest_0已经被设定为5'd0了
+// assign dest_o         = reg_valid && reg_gr_we ? reg_dest : 5'd0;
+
+assign ex_not_ready_o = reg_ex_not_ready;
+
+assign ex_allowin_o = !reg_valid || (ex_ready_go && mem_allowin_i);
+assign ex_to_mem_valid_o = reg_valid && ex_ready_go;
+
+assign data_sram_en_o    = reg_mem_en & ex_to_mem_valid_o & mem_allowin_i;
+assign data_sram_we_o    = reg_mem_we & {4{ex_to_mem_valid_o & mem_allowin_i}};
+assign data_sram_addr_o  = alu_result;
+assign data_sram_wdata_o = reg_rkd_value;
+
+assign ex_result_o       = {32{reg_alu_op != 12'd0}} & alu_result |
+                           {32{reg_mul_op !=  3'd0}} & mul_result |
+                           {32{reg_div_op !=  4'd0}} & div_result ;
+
+alu u_alu(
+    .alu_op     (reg_alu_op  ),
+    .alu_src1   (reg_cal_src1),
+    .alu_src2   (reg_cal_src2),
+    .alu_result (alu_result  )
+);
+
+mul u_mul(
+    .mul_op     (reg_mul_op  ),
+    .mul_src1   (reg_mul_src1),
+    .mul_src2   (reg_mul_src2),
+    .mul_result (mul_result  )
+);
+
+div u_div(
+    .clk        (clk_i       ),
+    .reset      (reset_i     ),
+    .div_start  (div_start   ),
+    .div_done   (div_done    ),
+    .div_op     (reg_div_op  ),  
+    .div_src1   (reg_cal_src1),
+    .div_src2   (reg_cal_src2),
+    .div_result (div_result  )
+);
+
+endmodule
---
+++ exp11/mycpu_preif.v
@@ -0,0 +1,49 @@
+module mycpu_preif(
+    input  wire        clk_i,
+    input  wire        reset_i,
+
+    // 输入
+    input  wire        br_taken_i,
+    input  wire [31:0] br_target_i,
+
+    // 给下一级的数据
+    output wire [31:0] pc_o,
+
+    // inst_ram读信号
+    output wire        inst_sram_en_o,
+    output wire [31:0] inst_sram_addr_o,
+
+    // 控制信号
+    input  wire        if_allowin_i,
+    output wire        pre_to_if_valid_o
+);
+
+wire        pre_ready_go = 1'b1;
+
+reg  [31:0] pc;
+wire [31:0] seq_pc;
+wire [31:0] nextpc;
+
+assign seq_pc       = pc + 32'h4;
+
+// 如果br_taken，则当然要变为br_target
+// 如果没有br_taken，且if阶段阻塞中，那么需要保持当前pc，跟着if一起阻塞
+assign nextpc = br_taken_i ? br_target_i : (if_allowin_i ? seq_pc : pc);
+
+always @(posedge clk_i) begin
+    if (reset_i) begin
+        pc <= 32'h1bfffffc;     //trick: to make nextpc be 0x1c000000 during reset 
+    end else if(if_allowin_i) begin
+        pc <= nextpc;
+    end
+end
+
+assign inst_sram_en_o   = ~reset_i;
+assign inst_sram_addr_o = nextpc;
+assign pc_o             = pc;
+
+// 确认跳转的当前周期的pc还是上一周期的nextpc，即seq_pc
+// 需等一周期pc才会变成br_target_i
+assign pre_to_if_valid_o = pre_ready_go & !br_taken_i;
+
+endmodule
